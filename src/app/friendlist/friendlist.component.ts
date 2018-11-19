f560b8221e5e0b12320aa1b72da23176177e8cd1
Saikat Singha Mahapatra <saikat.singhamahapatra@cognizant.com>
2018-11-19 18:09:32 +0530

    added edit profile

--- src/app/friendlist/friendlist.component.ts
+++ src/app/friendlist/friendlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-friendlist',
@@ -11,5 +12,7 @@ export class FriendlistComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  friends: User[] = [{id:1,username:"Beast_Whisperer",password:"12345"},{id:2,username:"Counter",password:"ram123"},{id:3,username:"saikat",password:"12345" },{id:4, username:"awptimusKs", password:"12345"}];
 
 }
